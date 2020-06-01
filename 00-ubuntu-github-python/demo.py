3b86e5b2816da15c3801b71c6646bd27b75a8c49
Eric Lu <eric565648@gmail.com>
2020-06-02 05:11:32 +0800

    polish

---
+++ 00-ubuntu-github-python/demo.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import numpy as numpy
+
+def function_demo():
+    print("This is the function")
+
+if __name__ == "__main__":
+    print("This is the main function")
+    function_demo()
+    print("function end")
