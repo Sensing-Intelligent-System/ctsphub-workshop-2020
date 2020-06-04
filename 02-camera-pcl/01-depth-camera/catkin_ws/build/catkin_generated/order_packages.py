7307514364d84876bb2cf9990a3ab133a9c163aa
WolfNCTU <andy11259@yahoo.com.tw>
2020-06-04 22:41:40 +0800

    change camera tutorial into rosbag tutorial

--- 02-camera-pcl/01-depth-camera/catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/arg/ctsphub-workshop-2020/02-camera-pcl/01-depth-camera/catkin_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/opt/ros/kinetic".split(';') if "/opt/ros/kinetic" != "" else []
--- 02-camera-pcl/01-depth-camera/catkin_ws/build/me212cv/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "me212cv"
-PROJECT_SPACE_DIR = "/home/arg/ctsphub-workshop-2020/02-camera-pcl/01-depth-camera/catkin_ws/install"
-PROJECT_VERSION = "0.0.0"
