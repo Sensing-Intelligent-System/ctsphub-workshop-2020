f09718a75e92fa85a5ce39d33a363cc5a0b91336
WolfNCTU <andy11259@yahoo.com.tw>
2020-06-05 23:50:15 +0800

    add rosbag tutorial

---
+++ 02-rosbag-pcl/01-rosbag/catkin_ws/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest
+# Build directory: /home/hcc2020/ctsphub-workshop-2020/02-rosbag-pcl/01-rosbag/catkin_ws/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
