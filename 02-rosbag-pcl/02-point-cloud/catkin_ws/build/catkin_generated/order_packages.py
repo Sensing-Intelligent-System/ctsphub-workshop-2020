492140281f61a2e646e4406f74e21e30f9be4069
WolfNCTU <andy11259@yahoo.com.tw>
2020-06-05 23:55:22 +0800

    delete build devel

--- 02-rosbag-pcl/02-point-cloud/catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/arg/ctsphub-workshop-2020/02-camera-pcl/02-point-cloud/catkin_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
