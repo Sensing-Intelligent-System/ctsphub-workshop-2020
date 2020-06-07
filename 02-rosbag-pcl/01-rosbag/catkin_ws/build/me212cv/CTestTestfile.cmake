91cf5958a8f5e554b8e43db9d6aae9965011f11f
WolfNCTU <andy11259@yahoo.com.tw>
2020-06-07 23:15:55 +0800

    delete build devel

--- 02-rosbag-pcl/01-rosbag/catkin_ws/build/me212cv/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hcc2020/ctsphub-workshop-2020/02-rosbag-pcl/01-rosbag/catkin_ws/src/me212cv
-# Build directory: /home/hcc2020/ctsphub-workshop-2020/02-rosbag-pcl/01-rosbag/catkin_ws/build/me212cv
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- 02-rosbag-pcl/01-rosbag/catkin_ws/build/me212cv/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "me212cv"
-PROJECT_SPACE_DIR = "/home/hcc2020/ctsphub-workshop-2020/02-rosbag-pcl/01-rosbag/catkin_ws/install"
-PROJECT_VERSION = "0.0.0"
