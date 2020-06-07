dd99c68ecfe845270daf5cb675b31ce85a3e7c07
WolfNCTU <andy11259@yahoo.com.tw>
2020-06-07 23:17:14 +0800

    update gitignorew

--- 02-rosbag-pcl/02-point-cloud/catkin_ws/build/catkin_generated/installspace/local_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-# generated from catkin/cmake/template/local_setup.sh.in
-
-# since this file is sourced either use the provided _CATKIN_SETUP_DIR
-# or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/home/hcc2020/ctsphub-workshop-2020/02-rosbag-pcl/02-point-cloud/catkin_ws/install}
-CATKIN_SETUP_UTIL_ARGS="--extend --local"
-. "$_CATKIN_SETUP_DIR/setup.sh"
-unset CATKIN_SETUP_UTIL_ARGS
--- 02-rosbag-pcl/02-point-cloud/catkin_ws/build/pcl_example/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hcc2020/ctsphub-workshop-2020/02-rosbag-pcl/02-point-cloud/catkin_ws/src/pcl_example
-# Build directory: /home/hcc2020/ctsphub-workshop-2020/02-rosbag-pcl/02-point-cloud/catkin_ws/build/pcl_example
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
