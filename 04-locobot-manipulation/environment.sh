6562ffc30e8584dd650d047e1f668dfa9bf20cad
jinglinjackychen <a2396660@gmail.com>
2020-06-14 19:03:25 +0800

    add 04-locobot-manipulation

---
+++ 04-locobot-manipulation/environment.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+source /opt/ros/melodic/setup.bash
+source ~/ctsphub-workshop-2020/05-locobot-navigation/catkin_ws/devel/setup.bash
+
+echo "ctsphub-workshop-2020 locobot navigation environment"
